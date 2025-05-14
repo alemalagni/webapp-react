4b9655b70ca954841ed0269f122933c67a545af4
alemalagni <alemalagni@gmail.com>
2025-05-14 12:50:31 +0200

    feta: add Movie function

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+
+function Movie() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+}
+
+export default Movie;
