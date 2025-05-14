69570653037c02efeaf2fee9150fd23865b168bc
alemalagni <alemalagni@gmail.com>
2025-05-14 13:02:29 +0200

    feat: add return

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -11,6 +11,35 @@ function Movie() {
             .then(res => setMovie(res.data))
             .catch(err => console.err('Errore nel caricamento del film:', err));
     }, [id]);
+
+    if (!movie) return <p>Caricamento...</p>;
+
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+            <p><strong>Regista:</strong></p>
+            <p><strong>Genere:</strong></p>
+            <p><strong>Anno:</strong></p>
+            <p>{movie.abstract}</p>
+            {movie.image && (
+                <img src={movie.image} alt={movie.title} className='img-fluid mb4' />
+            )}
+
+            <h3 className='mt-5'>Recensioni</h3>
+            {movie.reviews.length === 0 ? (
+                <p>Nessuna recensione disponibile</p>
+            ) : (
+                <ul className='list-group'>
+                    {movie.reviews.map(review => (
+                        <li key={review.id} className='list-group-item'>
+                            <strong>{review.name}</strong> - Voto: {review.vote}/5
+                            <p>{review.text}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default Movie;
