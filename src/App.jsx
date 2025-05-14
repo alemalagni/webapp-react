7fc6618f7d855cdf91db632ee7b06a452c9c92e4
alemalagni <alemalagni@gmail.com>
2025-05-14 13:03:56 +0200

    feat: add App script

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
 import './App.css'
 
 function App() {
