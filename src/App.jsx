cfcd25ec1384116e4822dc87e96fa8e67b206792
alemalagni <alemalagni@gmail.com>
2025-05-14 13:06:43 +0200

    feat: add return

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ import Movie from './pages/Movie';
 import './App.css'
 
 function App() {
-
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />} />
+                <Route path='movies/:id' element={<Movie />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App
+export default App;
