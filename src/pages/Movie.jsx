e0afb4fbd6ec186ba19277b7ac36a8ec28e6873f
alemalagni <alemalagni@gmail.com>
2025-05-14 12:53:04 +0200

    feat: add useEffect()

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,6 +5,12 @@ import axios from 'axios';
 function Movie() {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        axios.get(`http://localhost:300/movies/${id}`)
+            .then(res => setMovie(res.data))
+            .catch(err => console.err('Errore nel caricamento del film:', err));
+    }, [id]);
 }
 
 export default Movie;
