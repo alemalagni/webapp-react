8c001ba9c4cff25f35336d663987536e169b2bba
alemalagni <alemalagni@gmail.com>
2025-05-15 15:25:47 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 import './App.css'
