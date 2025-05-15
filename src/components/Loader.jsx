633906cc9b060f1544a97549131922d1ac58ba86
alemalagni <alemalagni@gmail.com>
2025-05-15 17:54:41 +0200

    feat: add return()

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,6 +5,13 @@ function Loader() {
 
     if (!isLoading) return null;
 
+    return (
+        <div className="loader-overlay">
+            <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Caricamento...</span>
+            </div>
+        </div>
+    )
 }
 
 export default Loader;
