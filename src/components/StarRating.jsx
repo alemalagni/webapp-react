e86195f0769a7ce8c89d3f0b608adf85db3b3a4c
alemalagni <alemalagni@gmail.com>
2025-05-15 15:41:05 +0200

    feat: add return()

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,6 +1,16 @@
 function StarRating() {
     const fullStar = '★';
     const emptyStar = '☆'
+
+    return (
+        <span>
+            {[...Array(5)].map((_, i) => (
+                <span key={i}>
+                    {i < rating ? fullStar : emptyStar}
+                </span>
+            ))};
+        </span>
+    );
 }
 
 export default StarRating;
