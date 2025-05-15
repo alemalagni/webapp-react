15136b957a33cece3ba56b9ac444712695d090f8
alemalagni <alemalagni@gmail.com>
2025-05-15 16:28:47 +0200

    feat: add handleSubmit

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -6,6 +6,22 @@ function ReviewForm({ movieId }) {
     const [vote, setVote] = useState(5);
     const [text, setText] = useState('');
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post(`http://localhost:3000/movies/${movieId}/reviews`, {
+                name,
+                vote,
+                text,
+            });
+
+            alert('Recensione inviata!')
+        } catch (err) {
+            console.error('Errore nel salvataggio:', err);
+            alert('Errore nel salvataggio della recensione.')
+        }
+    };
 }
 
 export default ReviewForm;
