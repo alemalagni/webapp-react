c1941e0b602442ca1e08f5b3efb246aaa7b76a57
alemalagni <alemalagni@gmail.com>
2025-05-15 16:24:08 +0200

    feat: add UseState

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { useState } from 'react';
 
 function ReviewForm({ movieId }) {
+    const [name, setName] = useState('');
+    const [vote, setVote] = useState(5);
+    const [text, setText] = useState('');
 
 }
 
